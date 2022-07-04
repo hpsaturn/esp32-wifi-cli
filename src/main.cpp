f01aafd47df799721df4c2e32a0a1ba28f700a2a
Hpsaturn <hpsaturn@gmail.com>
2022-07-04 19:06:57 +0200

    select default AP from list and load it, working

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,8 @@ Preferences cfg;
 String temp_ssid = "";
 String temp_pasw = "";
 
+String reconnect_mode = "last";
+
 void printWifiSettings() {
   Serial.print("\nWiFi SSID \t: [");
   Serial.println(WiFi.SSID()+"]");       //Output Network name.
@@ -47,20 +49,25 @@ void printWifiSettings() {
   Serial.println(WiFi.macAddress());     //Output MAC address.
   Serial.print("Hostname\t: ");
   Serial.println(WiFi.getHostname());       //Output hostname.
+  Serial.println("");
 }
 
 void printHelp(String opts) {
   Serial.println("\nUsage:\n");
-  Serial.println("setSSID     \t\"YOUR SSID\"");
-  Serial.println("setPassword \t\"YOUR PASSWORD\"");
-  Serial.println("connect");
-  Serial.println("scan");
-  Serial.println("status");
-  Serial.println("disconnect");
+  Serial.println("setSSID \"YOUR SSID\"\tset the SSID into quotes");
+  Serial.println("setPASW \"YOUR PASW\"\tset the password into quotes");
+  Serial.println("connect \t\tsave and connect to the network");
+  Serial.println("list \t\t\tlist all saved networks");
+  Serial.println("select <number>\t\tselect the default AP (default: last saved)");
+  Serial.println("mode <single/multi>\tset the connection mode. Multi AP is a little slow");
+  Serial.println("scan \t\t\tscan for available networks");
+  Serial.println("status \t\t\tprint the current WiFi status");
+  Serial.println("disconnect \t\tdisconnect from the network");
+  // Serial.println("delete <number>\t\t\tremove saved network");
+  Serial.println("help \t\t\tprint this help");
 }
 
 void scanNetworks(String opts) {
-  // WiFi.scanNetworks will return the number of networks found
   int n = WiFi.scanNetworks();
   Serial.print("\nscan done: ");
   if (n == 0) {
@@ -101,19 +108,26 @@ String getNetKeyName(int net) {
   return String(key);
 }
 
-void loadSavedNetworks() {
+void loadSavedNetworks(bool addAP) {
   cfg.begin("wifi_cli_prefs", RO_MODE);
   int net = 1;
-  Serial.println("\nLoading networks..");
+  int default_net = cfg.getInt("default_net", 1);
+  Serial.println("\nSaved networks:\n");
   while (cfg.isKey(String(getNetKeyName(net)+"_ssid").c_str())) {
     String key = getNetKeyName(net);
     String ssid = cfg.getString(String(key + "_ssid").c_str(), "");
     String pasw = cfg.getString(String(key + "_pasw").c_str(), "");
-    Serial.printf("%d: [%s][%s]\r\n", net, ssid.c_str(), pasw.c_str());
-    wifiMulti.addAP(ssid.c_str(), pasw.c_str());
+    String dfl = (net == default_net) ? "*" : " ";
+    Serial.printf("(%s) %d: [%s][%s]\r\n",dfl.c_str(), net, ssid.c_str(), pasw.c_str());
+    if(addAP) wifiMulti.addAP(ssid.c_str(), pasw.c_str());
     net++;
   }
+  Serial.println("");
   cfg.end();
+}
+
+void listNetworks(String opts) {
+  loadSavedNetworks(false);
 }
 
 bool isSSIDSaved(String ssid) {
@@ -125,6 +139,27 @@ bool isSSIDSaved(String ssid) {
     net++;
   }
   return false;
+}
+
+void deleteNetwork(String opts) {
+  maschinendeck::Pair<String, String> operands = maschinendeck::SerialTerminal::ParseCommand(opts);
+  int net = operands.first().toInt();
+  cfg.begin("wifi_cli_prefs", RW_MODE);
+  if (net > 0 && net < 100) {
+    String key = getNetKeyName(net);
+    if (!cfg.isKey(String(key + "_ssid").c_str())) {
+      Serial.println("\nNetwork not found");
+      cfg.end();
+      return;
+    }
+    // cfg.remove(String(key + "_ssid").c_str());
+    // cfg.remove(String(key + "_pasw").c_str());
+    Serial.printf("\nNetwork %s deleted", String(key+"_ssid").c_str());
+  } 
+  else {
+    Serial.println("\nInvalid network number");
+  }
+  cfg.end();
 }
 
 void saveNetwork(String ssid, String pasw) {
@@ -159,6 +194,81 @@ void disconnect(String opts) {
   WiFi.disconnect();
 }
 
+bool wifiValidation() {
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("connected!");
+    wifiStatus("");
+    return true;
+  } else {
+    Serial.println("connection failed!");
+    return false;
+  }
+}
+
+void wifiAPConnect(bool save) {
+  Serial.print("\nConnecting to " + temp_ssid + "...");
+  if(save) wifiMulti.addAP(temp_ssid.c_str(), temp_pasw.c_str());
+  int retry = 0;
+  WiFi.begin(temp_ssid.c_str(), temp_pasw.c_str());
+  while (WiFi.status() != WL_CONNECTED && retry++ < 20) {  // M5Atom will connect automatically
+    delay(1000);
+    Serial.print(".");
+  }
+  delay(100);
+  if (wifiValidation() && save) {
+    saveNetwork(temp_ssid, temp_pasw);
+  }
+}
+
+bool loadAP(int net) {
+  cfg.begin("wifi_cli_prefs", RO_MODE);
+  String key = getNetKeyName(net);
+  if (!cfg.isKey(String(key + "_ssid").c_str())) {
+    Serial.println("\nNetwork not found");
+    cfg.end();
+    return false;
+  }
+  Serial.printf("\nDefault AP: %i: [%s]\r\n", net, cfg.getString(String(key + "_ssid").c_str(), "").c_str());
+  temp_ssid = cfg.getString(String(key + "_ssid").c_str(), "");
+  temp_pasw = cfg.getString(String(key + "_pasw").c_str(), "");
+  cfg.end();
+  return true;
+}
+
+void selectAP(int net){
+  if(!loadAP(net)) return;
+  cfg.begin("wifi_cli_prefs", RW_MODE);
+  cfg.putInt("default_net", net);
+  cfg.end();
+}
+
+void selectAP(String opts) {
+  maschinendeck::Pair<String, String> operands = maschinendeck::SerialTerminal::ParseCommand(opts);
+  int net = operands.first().toInt();
+  selectAP(net);
+}
+
+int getDefaultAP() {
+  cfg.begin("wifi_cli_prefs", RO_MODE);
+  int net = cfg.getInt("default_net", 1);
+  cfg.end();
+  return net;
+}
+
+void setMode(String opts) {
+  maschinendeck::Pair<String, String> operands = maschinendeck::SerialTerminal::ParseCommand(opts);
+  cfg.begin("wifi_cli_prefs", RW_MODE);
+  if (operands.first().equals("single")) {
+    cfg.putString("mode", "last");
+  } else if (operands.first().equals("multi")) {
+    cfg.putString("mode", "multi");
+  } else {
+    Serial.println("\nInvalid mode, please use single/multi parameter");
+    printHelp(opts);
+  }
+  cfg.end();
+}
+
 void multiWiFiConnect() {
   int retry = 0;
   Serial.print("\nConnecting...");
@@ -166,13 +276,7 @@ void multiWiFiConnect() {
     delay(500);
     Serial.print(".");
   } 
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("connected!");
-    wifiStatus("");
-  }
-  else {
-    Serial.println("failed!");
-  }
+  wifiValidation();
 }
 
 void connect(String opts) {
@@ -184,28 +288,15 @@ void connect(String opts) {
     disconnect(opts);
     delay(1000);
   }
-  if(isSSIDSaved(temp_ssid) || temp_ssid.length() == 0) {
-    multiWiFiConnect();
+  if(isSSIDSaved(temp_ssid)) {
+    wifiAPConnect(false);
     return;
   }
+  else if(temp_ssid.length() > 0) {
+    wifiAPConnect(true); 
+  }
   else {
-    Serial.print("\nConnecting to " + temp_ssid + "...");
-    wifiMulti.addAP(temp_ssid.c_str(), temp_pasw.c_str());
-    int retry = 0;
-    WiFi.begin(temp_ssid.c_str(), temp_pasw.c_str());
-    while (WiFi.status() != WL_CONNECTED && retry++<20) { // M5Atom will connect automatically
-      delay(1000);
-      Serial.print(".");
-    }
-    delay(100);
-    if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("connected!");
-      saveNetwork(temp_ssid, temp_pasw);
-      wifiStatus(opts);
-    }
-    else {
-      Serial.println("connection failed!");
-    }
+    multiWiFiConnect(); //TODO: change to user selection
   }
 }
 
@@ -213,24 +304,25 @@ void setup() {
   M5.begin(true,false,true);  //Init Atom(Initialize serial port, LED)
   M5.dis.fillpix(0xffffff);   //Light LED with the specified RGB color. 
   WiFi.mode(WIFI_STA);
-  cfg.begin("wifi_cli_prefs", RO_MODE);
-  if (cfg.getInt("net_count", 0) >= 1) {
-    loadSavedNetworks();
-    multiWiFiConnect();
-  }
+  Serial.flush();
+  delay(1000);
+  Serial.println("\n\n");
+  loadSavedNetworks(true);
+  loadAP(getDefaultAP());
+  wifiAPConnect(false);
   cfg.end();
   delay(100);
-  Serial.flush();
-  Serial.println("\n\n");
   term = new maschinendeck::SerialTerminal(115200);
-  term->add("help", &printHelp, "\tshow help and usage information");
+  term->add("help", &printHelp, "\tshow detail usage information");
   term->add("setSSID", &setSSID, "\tset the Wifi SSID");
-  term->add("setPassword", &setPassword, "set the WiFi password"); 
-  term->add("connect", &connect, "\tconnect or add WiFi network");
+  term->add("setPASW", &setPassword, "\tset the WiFi password"); 
+  term->add("connect", &connect, "\tsave and connect to WiFi network");
+  term->add("list", &listNetworks, "\tlist saved WiFi networks");
+  term->add("select", &selectAP, "\tselect the default AP (default: last)");
+  term->add("mode", &setMode, "\tset the default operation single/multi AP (slow)");
   term->add("scan", &scanNetworks, "\tscan WiFi networks");
   term->add("status", &wifiStatus, "\tWiFi status information");
   term->add("disconnect", &disconnect, "WiFi disconnect");
- 
 }
 
 void loop() {
