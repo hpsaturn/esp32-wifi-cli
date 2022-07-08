d0089238bc74db1182ef5ff2d1bbed14756afd64
Hpsaturn <hpsaturn@gmail.com>
2022-07-08 22:09:53 +0200

    example with callback and custom commands implementations

---
+++ examples/advanced/main.cpp
@@ -0,0 +1,72 @@
+
+/*********************************************************************
+ This sample file is part of the esp32-wifi-cli:
+ Copyright (c) 2022, @hpsaturn, Antonio Vanegas
+ https://hpsaturn.com, All rights reserved.
+ https://github.com/hpsaturn/esp32-wifi-cli
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, version 3.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************/
+
+#include <ESP32WifiCLI.hpp>
+
+#define LED_BUILTIN 19
+
+class mESP32WifiCLICallbacks : public ESP32WifiCLICallbacks {
+  void onWifiStatus(bool isConnected) {
+    if(isConnected) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  }
+};
+
+void gotToSuspend(int type, int seconds) {
+    delay(8);  // waiting for writing msg on serial
+    //esp_deep_sleep(1000000LL * DEEP_SLEEP_DURATION);
+    esp_sleep_enable_timer_wakeup(1000000LL * seconds);
+    if (type == 0) esp_deep_sleep_start();
+    else esp_light_sleep_start(); 
+}
+
+void sleep(String opts) {
+  maschinendeck::Pair<String, String> operands = maschinendeck::SerialTerminal::ParseCommand(opts);
+  int seconds = operands.second().toInt();
+  if(operands.first().equals("deep")) {
+    Serial.println("\ndeep suspending..");
+    gotToSuspend(0, seconds);
+  }
+  else if(operands.first().equals("light")) {
+    Serial.println("\nlight suspending..");
+    gotToSuspend(1, seconds);
+  }
+  else {
+    Serial.println("sleep: invalid option");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.flush();
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(1000);
+  wcli.setCallback(new mESP32WifiCLICallbacks());
+  wcli.begin();
+  // custom commands:
+  wcli.term->add("sleep", &sleep, "\t<type> <time> ESP32 will enter to sleep mode");
+}
+
+void loop() {
+  wcli.loop();
+}
