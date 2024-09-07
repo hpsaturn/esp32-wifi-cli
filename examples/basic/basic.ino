48aa1a24184416f8244a133efd69f6d006e44dcc
Hpsaturn <hpsaturn@gmail.com>
2024-09-07 11:50:26 +0200

    removed old documentation of previous version

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -9,37 +9,12 @@
 /*********************************************************************
  * Basic example of ESP32WifiCLI.
  * 
- * After compiling and running this sketch, you can use the WiFi CLI
- * to connect to a WiFi network via the serial console or device monitor
+ * After compiling and running this sketch, you can use the network manager
+ * CLI to connect to your WiFi network via the serial console or device monitor.
+ * For details please type "nmcli help".
  * 
- * Enter to the console and type ENTER, reset your device and you should 
- * see something like the following:
-
-SerialTerm v1.1.2
-(C) 2022, MikO - Hpsaturn
-  available commands:
-        help            show detail usage information
-        setSSID         set the Wifi SSID
-        setPASW         set the WiFi password
-        connect         save and connect to WiFi network
-        list            list saved WiFi networks
-        select          select the default AP (default: last)
-        mode            set the default operation single/multi AP (slow)
-        scan            scan WiFi networks
-        status          WiFi status information
-        disconnect      WiFi disconnect
-        delete          remove saved WiFi network by SSID
-
-st>
-
-* To save and setup your first WiFi network, type:
-
-        setSSID "my-wifi-network"
-        setPASW "my-wifi-password"
-        connect
-
-* For advanced usage, you can review the advanced examples.
  ********************************************************************/
+
 #include <Arduino.h>
 #include <ESP32WifiCLI.hpp>
 
